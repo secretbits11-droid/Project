eaba024bf956a63098e24395142a32d7eea0707e
secretbits11-droid <secretbits11@gmail.com>
2026-09-20 23:44:29 +0100

    fix(build.gradle.kts): wrap android config and fix dependency syntax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
+android {
     namespace = "com.cloudgrip"
     compileSdk = 34
 
