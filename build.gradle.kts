1d27d916c68530b585324c72c8f97839ae668121
secretbits11-droid <secretbits11@gmail.com>
2026-09-20 21:34:17 +0100

    fix: resolve build failure by correcting plugin application and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
