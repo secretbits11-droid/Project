b3ce59787960176880e3f400e0562ce566fb80b6
secretbits11-droid <secretbits11@gmail.com>
2026-09-20 23:53:24 +0100

    fix: resolve gradle build configuration errors and synchronize dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.3.0"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
 }
+
+android {
     namespace = "com.cloudgrip"
     compileSdk = 34
 
