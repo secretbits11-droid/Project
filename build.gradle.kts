e452c397a748be7673a25f1783e13feb67a2222e
secretbits11-droid <secretbits11@gmail.com>
2026-09-21 01:59:07 +0100

    fix(cicd): add root plugins block and use gradle action for execution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,8 @@
 // Configuring JVM memory to prevent GC thrashing
 // Managed via gradle.properties: org.gradle.jvmargs=-Xmx2048m -XX:MaxMetaspaceSize=512m -XX:+UseG1GC
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
