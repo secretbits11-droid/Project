20a63d77f24c47190afe835f72098c3e4d2d5248
secretbits11-droid <secretbits11@gmail.com>
2026-09-21 13:16:31 +0100

    fix(build): move kotlin compilerOptions block outside of android block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-            freeCompilerArgs.add("-Xjvm-default=all")
-        }
-    }
     buildFeatures {
         compose = true
     }
@@ -49,6 +43,13 @@ android {
     lint {
         abortOnError = false
         checkReleaseBuilds = false
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        freeCompilerArgs.add("-Xjvm-default=all")
     }
 }
 
